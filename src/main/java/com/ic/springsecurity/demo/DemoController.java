754cb9e092167cf6b6d9c1e39820007c2effa792
AnhNH <anhnh11032001@gmail.com>
2023-12-30 11:28:22 +0700

    OpenApi 3 & Swagger UI integration

--- src/main/java/com/ic/springsecurity/demo/DemoController.java
+++ src/main/java/com/ic/springsecurity/demo/DemoController.java
@@ -1,6 +1,7 @@
 package com.ic.springsecurity.demo;
 
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/demo")
+@Hidden
 public class DemoController {
 
     @GetMapping
